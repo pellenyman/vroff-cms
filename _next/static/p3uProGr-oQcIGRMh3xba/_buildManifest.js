34aa8c3c4c8286865f904d9b1c5863bc9492c2f9
Pelle Nyman <pelle@weapply.se>
2026-02-19 05:59:30 +0100

    Updates

--- _next/static/p3uProGr-oQcIGRMh3xba/_buildManifest.js
+++ _next/static/p3uProGr-oQcIGRMh3xba/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/vroff-cms//_next/:path+",
-        "destination": "/vroff-cms/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
